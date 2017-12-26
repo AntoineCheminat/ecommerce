c132e1017e45a19136a01913344a7eab26d1c0bd
AntoineCheminat <a.cheminat@hotmail.com>
2017-12-26 05:20:30 +0100

    Class_Views_Cart

---
+++ resources/views/shop/cart.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app') @section('content')
+<div class="row">
+	@if(!Session::has('cart'))
+	<h3>Votre panier est vide</h3>
+	@else
+	<h3>Votre panier</h3>
+	<ul class="list-group">
+		@foreach($items as $item)
+		<li class="list-group-item">
+			<span>
+				<strong>{{$item['item']->title}}</strong> x {{$item['quantity']}}
+				<a href="{{route('product.reduce', ['id' => $item['item']->id])}}">
+					<i class="fa fa-minus-square" aria-hidden="true"></i>
+				</a>
+				<a href="{{route('product.delete', ['id' => $item['item']->id])}}">
+					<i class="fa fa-trash" aria-hidden="true"></i>
+				</a>
+			</span>
+			<span class="badge">{{$item['price']}} €</span>
+		</li>
+		@endforeach
+	</ul>
+	<span class="pull-right badge">Total à payer: {{$totalP}} €</span>
+	<a href="{{route('product.checkout')}}" class="btn btn-success">Passer au paiement</a>
+	@endif
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -92,3 +92,55 @@ Validation formulaire upload image
 
 Route::get('photo', 'PhotoController@create');
 Route::post('photo', 'PhotoController@store');
+
+
+/*
+
+PANIER !!!
+
+*/
+
+Route::get('/', [
+    'uses' => 'ProductController@index',
+    'as' => 'product.index'
+]);
+
+Route::get('/product/add/{id}', [
+    'uses' => 'ProductController@addItem',
+    'as' => 'product.add'
+]);
+
+Route::get('/product/cart', [
+    'uses' => 'ProductController@getCart',
+    'as' => 'product.cart'
+]);
+
+Route::get('/product/checkout', [
+    'uses' => 'ProductController@getCheckout',
+    'as' => 'product.checkout'
+]);
+
+Route::post('/product/checkout', [
+    'uses' => 'ProductController@postCheckout',
+    'as' => 'product.postCheckout'
+]);
+
+Route::get('/product/reducebyone/{id}', [
+    'uses' => 'ProductController@reduceByOne',
+    'as' => 'product.reduce'
+]);
+
+Route::get('/product/delete/{id}', [
+    'uses' => 'ProductController@deleteProduct',
+    'as' => 'product.delete'
+]);
+
+/*
+
+Auth
+
+*/
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
